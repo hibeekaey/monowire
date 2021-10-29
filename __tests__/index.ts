eebc5588ede847dc14b7082e3fb7756263e970d7
Ibukun Dairo <ibukun.o.dairo@gmail.com>
2021-10-29 18:31:18 +0100

    Setup testing and coverage

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,6 +1,10 @@
 import * as assert from "assert";
 import Application from "../src/application";
 
-it("should return the average of two numbers", () => {
+it("should return the average of two numbers", function () {
   assert.equal(Application.getAverage(2, 2), 2);
+});
+
+it("should create new `Application` instance", function () {
+  assert.ok(new Application());
 });
